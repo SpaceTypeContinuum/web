9fa41c9f676316dbb9065ba0c2dea80ff72a201d
Kevin Yeh <kevinyeah@utexas.edu>
2020-07-22 09:03:10 -0400

    fix sizing

--- assets/js/timer.js
+++ assets/js/timer.js
@@ -10,7 +10,6 @@ let nextp5
 
 function createSketch() {
     let currentSketch = sketches[nextSketchType].createSketch("canvas")
-    document.getElementById("container").style.background = sketches[nextSketchType].targetBg
     nextp5 = new p5(currentSketch, "canvas")
     lastSketchType = nextSketchType
     nextSketchType = (nextSketchType + 1) % sketches.length
@@ -18,16 +17,21 @@ function createSketch() {
 
 function startCycle() {
     let el
-    if (currentp5._elements[0].length > 0) {
+    if (currentp5._elements.length > 0) {
         el = currentp5._elements[0].canvas
     } else {
         el = document.querySelector("canvas:not([display=none])")
     }
     
     el.style.background = sketches[lastSketchType].targetBg
+    document.getElementById("container").style.background = sketches[nextSketchType].targetBg
     el.classList.add("fade-out")
+    setTimeout(startSketch, 3000)
+ }
+
+ function startSketch() {
     createSketch()
-    setTimeout(completeCycle, 3500)
+    setTimeout(completeCycle, 1000)
  }
 
  function completeCycle() {
@@ -41,5 +45,6 @@ function startCycle() {
 
 document.addEventListener("DOMContentLoaded", function(event) {
     createSketch()
+    document.getElementById("container").style.background = sketches[lastSketchType].targetBg
     completeCycle()
 })
